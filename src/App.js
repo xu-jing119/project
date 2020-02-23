74ea6bfe9dd9d44ac737f7c7e823c7e8b5594634
xujing <754130935@qq.com>
2020-02-23 16:37:56 +0800

    react框架搭建

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
+
+import Home from './pages/Home/index.js'
+import CityList from './pages/CityList/index.js'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+        <Link to='/home'>首页</Link>
+        </li>
+        <li>
+      <Link to='/citylist'>城市选择</Link>
+        </li>
+      </ul>
+      <Route path='/home' component={Home}></Route>
+      <Route path='/citylist' component={CityList}></Route>
     </div>
+    </Router>
   );
 }
 
